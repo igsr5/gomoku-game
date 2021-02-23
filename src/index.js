3ed1a66525aa77e48c24687ace3361d34145f68a
ichigo-dev <ichigo@web-freework.com>
2021-02-23 16:09:22 +0900

    fix class component => function

--- src/index.js
+++ src/index.js
@@ -2,49 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square">
+    {props.value}
+    </button>
+  );
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+function Board() {
+  function renderSquare(i) {
+    return <Square value={i} />;
   }
 
-  render() {
-    const status = 'Next player: X';
+  const status = 'Next player: X';
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-         {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
+  return (
+    <div>
+    <div className="status">{status}</div>
+    <div className="board-row">
+    {renderSquare(0)}
+    {renderSquare(1)}
+    {renderSquare(2)}
+    </div>
+    <div className="board-row">
+    {renderSquare(3)}
+    {renderSquare(4)}
+    {renderSquare(5)}
+    </div>
+    <div className="board-row">
+    {renderSquare(6)}
+    {renderSquare(7)}
+    {renderSquare(8)}
+    </div>
+    </div>
+  );
 }
 
-class Game extends React.Component {
-  render() {
+function Game() {
     return (
       <div className="game">
         <div className="game-board">
@@ -56,7 +51,6 @@ class Game extends React.Component {
         </div>
       </div>
     );
-  }
 }
 
 // ========================================
