fc907900f11aae31ceef92b9600d8528ba3c01b3
ichigo-dev <ichigo@web-freework.com>
2021-02-25 11:48:54 +0900

    add calculateWinner

--- src/index.js
+++ src/index.js
@@ -31,8 +31,33 @@ function Board() {
     return <Square value={squares[i]} onClick={ ()=>clickBtn(i) } />;
   }
 
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
-  const status = 'Next player: ' + isNext;
+  const winner = calculateWinner(squares)
+  var status
+  if (winner) {
+    status = 'Winner: ' + winner
+  } else {
+    status = 'Next player: ' + isNext;
+  }
 
   return (
     <div>
